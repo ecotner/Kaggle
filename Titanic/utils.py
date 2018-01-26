32955629bf642584a31828a8ef9527dd72f9c9be
ecotner <2.71828cotner@gmail.com>
2018-01-25 19:01:25 -0800

    added data preparation routine where each unique last name is new feature, for detecting families

--- Titanic/utils.py
+++ Titanic/utils.py
@@ -98,6 +98,76 @@ def process_data1(data, test=False):
     else:
         return data, PassengerId
 
+def process_data2(data, test=False):
+    '''
+    Fills in the blanks with averages. Also creates an extra set of features which is the person's last name.
+    
+    Drops the PassengerId/Ticket/Fare columns entirely because survival is unlikely to be based on them. Then drops the Cabin column because the vast majority of the data is missing. Finally, goes through and drops any other passenger who has missing data (a lot of people are missing entries in the Age column, but not too many; roughly 1/10 of the dataset). Also parses out the Survived column and uses that as the training labels.
+    
+    All columns are inherently numerical except Sex and Embarked. Sex can be considered a binary variable where (male, female) -> (0,1), and embarked will be treated as (C,Q,S) -> (0,1,2)
+    '''
+    import numpy as np
+#    import pandas as pd
+        
+    # Drop the unwanted columns. Those that are left are:
+    #   Survived/Pclass/Name/Sex/Age/SibSp/Parch/Embarked
+    if not test:
+        names = data['Name'].copy().values
+        data = data[['Survived','Pclass','Sex','Age','SibSp','Parch','Embarked']].copy()
+    else:
+        PassengerId = data['PassengerId'].copy().values
+        names = data['Name'].copy().values
+        data = data[['Pclass','Sex','Age','SibSp','Parch','Embarked']].copy()
+    
+    # Map Sex/Embarked columns to numerical values
+    sex = np.where(data['Sex'] == 'male', 0, 1)
+    data['Sex'] = sex
+    embarked = np.where(data['Embarked'] == 'C', 0, data['Embarked'])
+    embarked = np.where(embarked == 'Q', 1, embarked)
+    embarked = np.where(embarked == 'S', 2, embarked)
+    data['Embarked'] = embarked
+    data = np.array(data, dtype=float)
+    
+    # Get indices of all rows which have missing data
+    non_missing_data_row_idxs = []
+    for row in range(data.shape[0]):
+        if not np.any(np.isnan(data[row,:])):
+            non_missing_data_row_idxs.append(row)
+    
+    # Make temp dataset without missing stuff to compute averages
+    data_ = data[non_missing_data_row_idxs,:]
+    data_ = np.mean(data_, axis=0)
+    
+    # Replace missing features with averages
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            if np.isnan(data[i,j]):
+                data[i,j] = data_[j]
+    
+    # Create new set of features which is one-hot vector representing person's last name.
+    names_dict = {}
+    names_dimension = []
+    for name in names:
+        surname = name.split(',')[0].lower()
+        if surname in names_dict:
+            names_dimension.append(names_dict[surname])
+        else:
+            names_dict[surname] = len(names_dict)
+            names_dimension.append(names_dict[surname])
+    names_features = np.zeros(shape=[len(names_dimension), len(names_dict)])
+    for i, j in enumerate(names_dimension):
+        names_features[i,j] = 1
+    # Append names features to data
+    data = np.concatenate([data, names_features], axis=1)
+    
+    # Separate out input labels and data
+    if not test:
+        x = data[:,1:]
+        y = data[:,0].astype(int)
+        return x, y
+    else:
+        return data, PassengerId
+
 def scale_data(data, sparse_feature_idxs=[]):
     ''' Scales the data to zero mean and unit variance, unless some features are identified as sparse, then it just scales to unit variance. Assumes data is a rank-2 numpy array with batch dimension on axis 0. '''
     import numpy as np
@@ -109,7 +179,7 @@ def scale_data(data, sparse_feature_idxs=[]):
         else:
             mean = np.average(data[:,column])
         std = np.std(data[:,column])
-        assert std != 0, 'Zero variance detected in data scaling!'
+        assert std != 0, 'Zero variance detected in column {} while data scaling!'.format(column)
         mean_list.append(mean)
         std_list.append(std)
         data[:,column] = (data[:,column] - mean)/std
@@ -126,7 +196,7 @@ if __name__ == '__main__':
     
     TRAIN_DATA_PATH = '../Datasets/Titanic/train.csv'
     data = pd.read_csv(TRAIN_DATA_PATH)
-    X_train, Y_train = process_data0(data)
+    X_train, Y_train = process_data2(data)
     print(X_train)
 
 
