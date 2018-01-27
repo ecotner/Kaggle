7f7884305995d34c07c6f0841499800de74711da
ecotner <2.71828cotner@gmail.com>
2018-01-27 13:35:19 -0800

    updated predictions

--- Titanic/train.py
+++ Titanic/train.py
@@ -10,28 +10,34 @@ Cleans the data and trains the algorithm on an ensemble of SVM
 import numpy as np
 import pandas as pd
 import utils as u
+import time
 from sklearn import svm
 from sklearn.neural_network import MLPClassifier as MLP
 #from mlp import MLP
 
-# Set up important macros
-TRAIN_DATA_PATH = '../Datasets/Titanic/train.csv'
-TEST_DATA_PATH = '../Datasets/Titanic/test.csv'
-#VAL_FRACTION = 0.25
-
-# Import the data into a DataFrame
-train_data = pd.read_csv(TRAIN_DATA_PATH)
-m_train = train_data.shape[0]
-test_data = pd.read_csv(TEST_DATA_PATH)
-all_data = pd.concat([train_data, test_data])
-
-# Clean the data, shuffle it randomly, scale it, and split into train/test
-X, Y = u.process_data2(all_data)
-X, mean, std = u.scale_data(X)
+## Set up important macros
+#TRAIN_DATA_PATH = '../Datasets/Titanic/train.csv'
+#TEST_DATA_PATH = '../Datasets/Titanic/test.csv'
+##VAL_FRACTION = 0.25
+#
+## Import the data into a DataFrame
+#train_data = pd.read_csv(TRAIN_DATA_PATH)
+#m_train = train_data.shape[0]
+#test_data = pd.read_csv(TEST_DATA_PATH)
+#all_data = pd.concat([train_data, test_data])
+#
+## Clean the data, shuffle it randomly, scale it, and split into train/test
+#X, Y = u.process_data3(all_data)
+X = np.load('./X.npy')
+Y = np.load('./Y.npy')
+m_train = 891
+print('X.shape = {}'.format(X.shape))
+#X, mean, std = u.scale_data(X)
 X_train, Y_train = X[:m_train], Y[:m_train]
-np.random.seed(1)
+seed = int(time.time())
+np.random.seed(seed)
 X_train = np.random.permutation(X_train)
-np.random.seed(1)
+np.random.seed(seed)
 Y_train = np.random.permutation(Y_train)
 X_test = X[m_train:]
 PassengerId = np.arange(m_train+1, all_data.shape[0]+1)
@@ -51,15 +57,24 @@ with open('./Titanic.log', 'w+') as fo:
 
 # Build models using k-fold cross-validation
 model = {}
-N_MODELS = 2
-K_FOLDS = 4
+N_MODELS = 7
+K_FOLDS = 10
 for k in range(K_FOLDS):
-    model[N_MODELS*k+0] = svm.SVC(kernel='rbf', C=1e2, gamma=9e-4, probability=False)
-    model[N_MODELS*k+1] = svm.SVC(kernel='linear', C=9e-2, gamma=.001, probability=False)
-#    model[N_MODELS*k+2] = svm.SVC(kernel='poly', degree=2, C=1e3, gamma=.001, probability=False)
-#    model[N_MODELS*k+3] = svm.SVC(kernel='poly', degree=3, C=3e5, gamma=.0001, probability=False)
-#    model[N_MODELS*k+4] = svm.SVC(kernel='sigmoid', C=2e0, gamma=.001, coef0=0, probability=False)
-#    model[N_MODELS*k+5] = MLP(solver='adam', learning_rate='constant', learning_rate_init=3e-3, alpha=1.5e1, hidden_layer_sizes=(50,20), activation='relu')
+#    model[N_MODELS*k+0] = svm.SVC(kernel='rbf', C=3e2, gamma=1.5e-3)
+#    model[N_MODELS*k+1] = svm.SVC(kernel='linear', C=3e0, gamma='auto')
+#    model[N_MODELS*k+2] = svm.SVC(kernel='poly', degree=2, C=1e3, gamma=1e-3)
+#    model[N_MODELS*k+3] = svm.SVC(kernel='poly', degree=3, C=1e3, gamma=1.2e-3)
+#    model[N_MODELS*k+4] = svm.SVC(kernel='sigmoid', C=1.3e3, gamma=1e-5, coef0=-0.3)
+#    model[N_MODELS*k+5] = MLP(solver='lbfgs', learning_rate='adaptive', learning_rate_init=3e-3, alpha=2e0, hidden_layer_sizes=(35,20), activation='relu')
+#    model[N_MODELS*k+6] = MLP(solver='lbfgs', learning_rate='adaptive', learning_rate_init=1e-2, alpha=3e1, hidden_layer_sizes=(15,60), activation='relu')
+    
+    model[N_MODELS*k+0] = svm.SVC(kernel='rbf', C=1e4, gamma=1e-5)
+    model[N_MODELS*k+1] = svm.SVC(kernel='linear', C=3e0, gamma=1e-3)
+    model[N_MODELS*k+2] = svm.SVC(kernel='poly', degree=2, C=6e3, gamma=1e-4) # C=6e3, gamma=1e-4 -> .861
+    model[N_MODELS*k+3] = svm.SVC(kernel='poly', degree=3, C=1.2e4, gamma=1.2e-4) # C=1.2e4, gamma=1.2e-4 -> .853
+    model[N_MODELS*k+4] = svm.SVC(kernel='sigmoid', C=3e4, gamma=.6e-5, coef0=0) # C=3e4, gamma=.6e-5 -> .840
+    model[N_MODELS*k+5] = MLP(solver='lbfgs', learning_rate='adaptive', learning_rate_init=9e-3, alpha=3e1, hidden_layer_sizes=(30), activation='relu')
+    model[N_MODELS*k+6] = MLP(solver='lbfgs', learning_rate='adaptive', learning_rate_init=1e-2, alpha=3e1, hidden_layer_sizes=(20,60), activation='relu')
 
 # Train and validate models
 model_train_acc_lists = {i:[] for i in range(N_MODELS)}
@@ -91,11 +106,11 @@ for idx in range(N_MODELS):
 #print('Ensemble accuracy: {}'.format(ens_accuracy))
 
 # Rerun training on the ENTIRE training set w/o changing parameters to get as much out of it as possible
-for idx in model:
+for idx in range(N_MODELS):
     model[idx].fit(X_train, Y_train)
 
 # Run predictions on the test set
-predictions = np.stack([model[i].predict(X_test).squeeze() for i in model]).T
+predictions = np.stack([model[idx].predict(X_test).squeeze() for idx in range(N_MODELS)]).T
 ens_predictions = (np.mean(predictions, axis=1) >= 0.5).astype(int)
 
 # Output predictions to CSV
